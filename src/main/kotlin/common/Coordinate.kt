a23671665db0236f7f26d6c9a0f5fb0e332c1845
joozd <euroshopper@gmail.com>
2021-11-29 15:38:21 +0100

    Getting ready for day 1

---
+++ src/main/kotlin/common/Coordinate.kt
@@ -0,0 +1,23 @@
+package common
+
+/**
+ * because Pairs are too confusing for Joozd
+ * Hash is unreliable for x and y values above 2^16
+ */
+
+open class Coordinate(val x: Int, val y: Int): Comparable<Coordinate>{
+    override fun equals(other: Any?) = if (other !is Coordinate) false else
+        other.x == x && other.y == y
+    override fun toString() = "Coordinate ($x, $y)"
+
+    override fun hashCode(): Int  = x.shl(16) + y
+
+    /**
+     *  top to bottom, left to right (y=0 is top row)
+     *  so in order:
+     *  1,2,3
+     *  4,5,6
+     *  7,8,9
+     */
+    override fun compareTo(other: Coordinate): Int = if (y == other.y) x-other.x else y - other.y
+}
