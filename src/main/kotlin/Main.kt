fd8cb3e9ddff9d7e467c3f1c6a411ddc71415d58
joozd <euroshopper@gmail.com>
2021-12-01 07:00:50 +0100

    Getting ready for day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     when (readLine()) {
         "0" -> day0.Day0().runTimed()
         "1" -> day1.Day1().runTimed()
-        //"2" -> day2.Day2().runTimed()
+        "2" -> day2.Day2().runTimed()
         //"3" -> day3.Day3().runTimed()
         //"4" -> day4.Day4().runTimed()
         //"5" -> day5.Day5().runTimed()
---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,11 @@
+package day2
+
+import common.Solution
+
+class Day2: Solution {
+    override val day = 2
+
+    override fun answer1(): Any = readInput()
+
+    override fun answer2(): Any = inputLines().first()
+}
