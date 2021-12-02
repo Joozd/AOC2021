555bea55c7ba3c5e68cdc8e3f546a35524e08d6f
Joozd <joozd@ius.org>
2021-12-02 20:38:53 +0100

    Ready for day 3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,11 @@
+package day3
+
+import common.Solution
+
+class Day3: Solution {
+    override val day = 3
+
+    override fun answer1(): Any = readInput()
+
+    override fun answer2(): Any = inputLines().first()
+}
