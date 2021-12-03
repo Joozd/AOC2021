4d685ac8bedccf8c07c9a7f766aa6276ad60b704
Joozd <joozd@ius.org>
2021-12-03 13:18:56 +0100

    Prepared for day 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ fun main() {
         "1" -> day1.Day1().runTimed()
         "2" -> day2.Day2().runTimed()
         "3" -> day3.Day3().runTimed()
-        //"4" -> day4.Day4().runTimed()
+        "4" -> day4.Day4().runTimed()
         //"5" -> day5.Day5().runTimed()
         //"6" -> day6.Day6().runTimed()
         //"7" -> day7.Day7().runTimed()
---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,13 @@
+package day4
+
+import common.Solution
+
+class Day4: Solution {
+    override val day = 0
+
+    // override fun prepare(){ super.prepare() }
+
+    override fun answer1(): Any = readInput()
+
+    override fun answer2(): Any = inputLines().first()
+}
