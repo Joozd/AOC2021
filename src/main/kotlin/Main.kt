519988ae7d150925aa20cc90c4338edaf2bd3047
joozd <euroshopper@gmail.com>
2021-12-07 18:40:59 +0100

    Prepared for day 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("Welcome to Joozd's AOC 2018 solution runner.")
+    println("Welcome to Joozd's AOC 2021 solution runner.")
     println("Please tell us what day you want to run: ")
     when (readLine()) {
         "0" -> day0.Day0().runTimed()
@@ -10,12 +10,13 @@ fun main() {
         "5" -> day5.Day5().runTimed()
         "6" -> day6.Day6().runTimed()
         "7" -> day7.Day7().runTimed()
-        //"8" -> day8.Day8().runTimed()
+        "8" -> day8.Day8().runTimed()
         //"9" -> day9.Day9().runTimed()
         //"10" -> day10.Day10().runTimed()
         //"11" -> day11.Day11().runTimed()
         //"12" -> day12.Day12().runTimed()
         //"13" -> day13.Day13().runTimed()
         //"14" -> day14.Day14().runTimed()
+        else -> println("Day not found, exiting")
     }
 }
---
+++ src/main/kotlin/day8/Day8.kt
@@ -0,0 +1,11 @@
+package day8
+
+import common.Solution
+
+class Day8: Solution {
+    override val day = 8
+
+    override fun answer1(): Any = "one"
+
+    override fun answer2(): Any = "two"
+}
