4a965ddec2d076f75f55e9136169ea941df7e381
Joozd <joozd@ius.org>
2021-12-07 11:47:04 +0100

    Added some comments

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -4,6 +4,10 @@ import common.Solution
 import common.extensions.abs
 import common.extensions.grabInts
 
+/**
+ * Calculate the minimum total fuel use from a list of points on a line to a target
+ * Decided to just brute-force it. Looking for a minimum would be faster, but this already takes < 0,1 sec on my laptop.
+ */
 class Day7: Solution {
     override val day = 7
 
@@ -12,11 +16,17 @@ class Day7: Solution {
     private val possibleTargets = (0..maxPosition)
 
 
+    /**
+     * Linear fuel use: fuel = distance
+     */
     override fun answer1(): Any =
         possibleTargets.minOf { t ->
             crabs.distancesFromTarget(t).sum()
         }
 
+    /**
+     * Increasing fuel use: fuel is (1+2+3+...+distance)
+     */
     override fun answer2(): Any =
         possibleTargets.minOf { t ->
             crabs.incrementingFuelBurnDistancesToTarget(t).sum()
@@ -29,5 +39,8 @@ class Day7: Solution {
     private fun List<Int>.incrementingFuelBurnDistancesToTarget(target: Int) =
         map { getIncrementedFuelUse((it-target).abs()) }
 
+    /**
+     * @return (1+2+3+...+steps)
+     */
     private fun getIncrementedFuelUse(steps: Int) = (steps * (steps + 1))/2
 }
