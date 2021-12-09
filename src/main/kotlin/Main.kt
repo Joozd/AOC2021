d74e113e32304a1b1270970767ad2792f4e96959
Joozd <joozd@ius.org>
2021-12-09 19:49:45 +0100

    Prepared for day 10

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ fun main() {
         "7" -> day7.Day7().runTimed()
         "8" -> day8.Day8().runTimed()
         "9" -> day9.Day9().runTimed()
-        //"10" -> day10.Day10().runTimed()
+        "10" -> day10.Day10().runTimed()
         //"11" -> day11.Day11().runTimed()
         //"12" -> day12.Day12().runTimed()
         //"13" -> day13.Day13().runTimed()
---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,11 @@
+package day10
+
+import common.Solution
+
+class Day10: Solution {
+    override val day = 10
+
+    override fun answer1(): Any = "one"
+
+    override fun answer2(): Any = "two"
+}
