434a75e0f486434850c629d4d5e89e9668907f86
Joozd <joozd@ius.org>
2021-12-21 18:11:48 +0100

    added documentation

--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -8,10 +8,12 @@ import kotlin.math.max
 class Day21: Solution {
     override val day = 21
     private val input = inputLines().map {it.words().last().toInt()}
-    private val testInput = listOf(4,8)
 
     private val diracDice = DiracDice(3)
 
+    /**
+     * A game with a 100 sided deterministic dice
+     */
     override fun answer1(): Any {
         val game = GameState(input[0], input[1], 1000)
         val dice = Dice()
@@ -19,8 +21,6 @@ class Day21: Solution {
         while(!game.hasWinner()){
             game.addRoll(0, dice.roll())
             if (game.hasWinner()) {
-                println("player 1 won: $game")
-                println("Rolls: ${dice.rolls}")
                 return game.score(dice.rolls)
             }
             game.addRoll(1, dice.roll())
@@ -28,6 +28,9 @@ class Day21: Solution {
         return game.score(dice.rolls)
     }
 
+    /**
+     * All possible games with 3 3-sided dice
+     */
     override fun answer2(): Any {
         val initialGameState = GameState(input[0], input[1], 21)
         return playDiracDiceMultithreaded(initialGameState, true).let{
@@ -51,18 +54,22 @@ class Day21: Solution {
      * Else, play another round. and return the results of that round.
      */
     private fun getResultForRoll(gameState: GameState, playerOnesTurn: Boolean, roll: Int): Pair<Long, Long>{
-       // println(gameState)
         gameState.addRoll(if (playerOnesTurn) 0 else 1, roll)
         return if (gameState.hasWinner()){
-            //println("winning game:\n$gameState")
             if (gameState.playerOneWon()) 1L to 0L else 0L to 1L
         }
         else playDiracDice(gameState, !playerOnesTurn)
     }
 
+    /**
+     * Functions to add and multiply Pairs
+     */
     private operator fun Pair<Long, Long>.times(n: Int) = this.first * n to this.second * n
     private operator fun Pair<Long, Long>.plus(other: Pair<Long, Long>) = this.first + other.first to this.second + other.second
 
+    /**
+     * Sum all values in pairs (sum all firsts and all seconds)
+     */
     private fun List<Pair<Long, Long>>.sum() = this.reduce { acc, pair -> acc + pair }
 
 }
--- src/main/kotlin/day21/GameState.kt
+++ src/main/kotlin/day21/GameState.kt
@@ -1,33 +1,56 @@
 package day21
 
+/**
+ * Keep track of a game's state
+ */
 class GameState(private var p1: Int, private var p2: Int, private val endOfGame: Int = 1000, private var s1: Int = 0, private var s2: Int = 0){
+    /**
+     * Copy this games state so two separate games can continue from this point
+     */
     fun copy() = GameState(p1, p2, endOfGame, s1,s2)
-    override fun toString() = "p1, s1 / p2, s2"
+
+    /**
+     * Check if this game has a winner
+     */
     fun hasWinner() = listOf(0,1).any { isWinner(it) }
 
     /**
+     * Check if player 1 won.
      * This does not check if there IS a winner, if there isn't it will return 2
      */
     fun playerOneWon(): Boolean = isWinner(0)
 
+    /**
+     * Add a dice roll result to this gameState
+     */
     fun addRoll(player: Int, value: Int){
         if(player == 1)
             roll1(value)
         else roll2(value)
     }
+
+    // add the roll to player 1
     private fun roll1(value: Int){
         val r = p1 + value
         p1 = if (r == 10) 10 else r%10
         s1 += p1
     }
+
+    //add the roll to player 2
     private fun roll2(value: Int){
         val r = p2 + value
         p2 = if (r == 10) 10 else r%10
         s2 += p2
     }
 
-    fun score(rolls: Int) = (if (isWinner(0)) s1 else s2) * rolls
+    /**
+     * Get the final score for this game
+     */
+    fun score(rolls: Int) = (if (playerOneWon()) s1 else s2) * rolls
 
+    /**
+     * Check if player 0 or 1 is winner
+     */
     private fun isWinner(player: Int): Boolean = if(player == 1) s1 >= endOfGame else s2 >= endOfGame
 
 }
